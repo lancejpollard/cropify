1a47eac43f70ba11ba59676821eae7e891d0772c
Lance Pollard <lancejpollard@gmail.com>
2010-04-05 08:09:09 -0700

    multiple file uploads

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-	$("#add-size").click(function() {
-		$("#cropify-table-body").append($("#cropify-table-body tr:first").clone());
+	$("input.add").live("click", function() {
+		$("#cropify-table-body").append($(this).parent().parent().clone());
+		return false;
+	});
+	$("input.remove").live("click", function() {
+		$(this).parent().parent().remove();
+		return false;
 	});
 	$('input[type=file]').change(function(e){
-		// $("#files").append("<div class='file'>" + $(this).val() + "</div>");
+		amount = this.files.length;
+		if (amount == 1)
+			amount = "1 File"
+		else
+			amount = amount.toString() + " Files"
+		$("#files .label").html("<p class='file'>" + amount + "</p>");
 	});
 //		$('.text_input').scale9Grid({top:15,bottom:15,left:15,right:15});
 })
