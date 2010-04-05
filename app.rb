740f94e0302d4a9b4993d60e80a43b045a64435d
Lance Pollard <lancejpollard@gmail.com>
2010-04-05 02:34:08 -0700

    works with registration point and fill/fit/chop

--- app.rb
+++ app.rb
@@ -11,68 +11,72 @@ set :views, "views"
 configure :production do
 end
 
-# http://github.com/henrik/slugalizer/blob/master/slugalizer.rb
-module Sluggify
-  extend self
-  
-  SEPARATORS = %w[- _ +]
-  
-  def sluggify(separator = "-")
-    unless SEPARATORS.include?(separator)
-      raise "Word separator must be one of #{SEPARATORS}"
+module Cropify
+  class Image
+    attr_accessor :width, :height, :name, :type, :ext, :quality, :gravity, :content
+    
+    def initialize(attributes = {})
+      attributes.each {|k,v| self.send "#{k}=", v} unless attributes.nil?
     end
-    re_separator = Regexp.escape(separator)
-    result = decompose
-    result.gsub!(/[^\x00-\x7F]+/, '')                      # Remove non-ASCII (e.g. diacritics).
-    result.gsub!(/[^a-z0-9\-_\+]+/i, separator)            # Turn non-slug chars into the separator.
-    result.gsub!(/#{re_separator}{2,}/, separator)         # No more than one of the separator in a row.
-    result.gsub!(/^#{re_separator}|#{re_separator}$/, '')  # Remove leading/trailing separator.
-    result.downcase!
+    
+    def detailed_name
+      "#{name}-#{width}wx#{height}hx#{quality}q#{ext}"
+    end
+    
+    def inspect
+      "#<Cropify:Image @detailed_name=#{self.detailed_name.inspect} @width=#{self.width.to_s.inspect} @height=#{self.height.to_s.inspect}/>"
+    end
+  end
+  
+  def self.crop(file, sizes)
+    result           = []
+    image            = Magick::Image.read(file[:tempfile].path).first
+    sizes.each do |size|
+      scale_mode     = size["scale_mode"]
+      quality        = [size["quality"].to_i, 100].min
+      gravity_type   = "Magick::#{size["gravity_type"].camelize}Gravity".constantize if size["gravity_type"].downcase != "none"
+      x              = size.has_key?("x") ? size["x"].to_i : 0
+      y              = size.has_key?("y") ? size["y"].to_i : 0
+      width          = size.has_key?("width") ? size["width"].to_i : image.columns
+      height         = size.has_key?("height") ? size["height"].to_i : image.rows
+      processed      = nil
+      if scale_mode == "chop"
+        if gravity_type
+          processed  = image.crop(gravity_type, x, y, width, height)
+        else
+          processed  = image.crop(x, y, width, height)
+        end
+      elsif scale_mode == "fit"
+        processed    = image.resize_to_fit(width, height)
+      else # fill
+        if gravity_type
+          processed  = image.resize_to_fill(width, height, gravity_type)
+        else
+          processed  = image.resize_to_fill(width, height)
+        end
+      end
+      result << Cropify::Image.new(
+        :quality => quality,
+        :width => processed.columns,
+        :height => processed.rows,
+        :gravity => size["gravity_type"],
+        :name => File.basename(file[:filename]).gsub(/#{File.extname(file[:filename])}$/, ""),
+        :ext => File.extname(file[:filename]),
+        :type => file[:type],
+        :content => quality != 0 ? processed.to_blob {self.quality = quality} : processed.to_blob
+      )
+    end
     result
   end
-  
-private
- 
-  def decompose
-    if defined?(ActiveSupport::Multibyte::Handlers)  # Active Support <2.2
-      ActiveSupport::Multibyte::Handlers::UTF8Handler.normalize(self, :kd).to_s
-    else  # ActiveSupport 2.2+
-      ActiveSupport::Multibyte::Chars.new(self).normalize(:kd).to_s
-    end
-  end
-  
-end
-
-class String
-  include Sluggify
-end
-
-def file_name(name)
-  ext = File.extname(name)
-  "#{Time.now.to_s}-#{name}-#{rand(10000).to_s}".sluggify + ext
-end
-
-def crop(file, sizes)
-  result           = []
-  image            = Magick::Image.read(file[:tempfile].path).first
-  sizes.each do |size|
-    x              = size.has_key?("x") ? size["x"].to_i : 0
-    y              = size.has_key?("y") ? size["y"].to_i : 0
-    width          = size.has_key?("width") ? size["width"].to_i : image.columns
-    height         = size.has_key?("height") ? size["height"].to_i : image.rows
-    result         << image.crop(x, y, width, height)
-  end
-  result
 end
 
 def download_zip(files, sizes)
-  
   return [] if files.nil? || files.empty? || sizes.nil? || sizes.empty?
-  
+
   files.each do |file|
-    file[:results] = crop(file, sizes)
+    file[:results] = Cropify.crop(file, sizes)
   end
-  
+
   file_name = params["folder"] || "cropify-images.zip"
   t = Tempfile.new("cropify-tempfiles-#{Time.now}-#{rand(10000)}")
 
@@ -81,8 +85,8 @@ def download_zip(files, sizes)
       ext = File.extname(file[:filename])
       basename = File.basename(file[:filename])
       file[:results].each do |cropped|
-        z.put_next_entry("#{basename}-#{cropped.columns}x#{cropped.rows}#{ext}")
-        z.print cropped.to_blob # IO.read(file[:tempfile].path)
+        z.put_next_entry(cropped.detailed_name)
+        z.print cropped.content # IO.read(file[:tempfile].path)
       end
     end
   end
@@ -99,5 +103,5 @@ get "/" do
 end
 
 post "/" do
-  zip = download_zip(params["files"], params["sizes"])
+  zip = download_zip(params["files"], params["cropfiy"])
 end
